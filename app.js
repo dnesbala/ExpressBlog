ab32687c51805a6884788e86841a22c4891f88ae
Dinesh <baladnes@gmail.com>
2020-07-10 21:21:19 +0545

    Initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const blogRoutes = require("./routes/blogRoutes");
+
+const app = express();
+
+const dbURI =
+  "mongodb+srv://test:test1234@cluster0-uowvc.mongodb.net/node-ninja?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(app.listen(3000))
+  .catch((err) => console.log(err));
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+// app.use(morgan("dev"));
+
+// Body parser
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.redirect("/blogs");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
+});
+
+// Blog Routes
+app.use("/blogs", blogRoutes);
+
+app.use((req, res) => {
+  res.status(404).render("404", { title: "Page not found" });
+});
